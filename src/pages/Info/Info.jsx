9871f626f756c551bb4923f46628d9be753827de
matheusDrumond <drumond.matheusc@gmail.com>
2023-08-31 21:52:50 -0300

    Bug resolution, and CSS finished

--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -1,4 +1,5 @@
 // Styles 
+import { useLayoutEffect, useState } from 'react'
 import './Info.css'
 
 // React Router
@@ -7,56 +8,58 @@ import { useParams } from 'react-router-dom'
 const Info = ({ carsInfo }) => {
     const { id } = useParams()
 
-    const car = carsInfo[id]
+    const [car, setCar] = useState(null)
 
-    const {
-      model, 
-      make, 
-      year, 
-      fuel_type, 
-      drive, 
-      cylinders, 
-      transmission, 
-      city_mpg, 
-      highway_mpg,
-    } = car
+    useLayoutEffect(()=>{
+      carsInfo && setCar(carsInfo[id])
+
+    }, [carsInfo])
+
 
   return (
     <div className='car-info'>
-      <div className="info-header">
-        <h1 className='info-car-model'>{model}</h1>
-        <span className='info-car-make'>{make}</span>
-        <span className='info-car-year'>{year}</span>
+      {car && <>
+        <div className="info-header">
+        <h1 className='info-car-model'>{car.model}</h1>
+        <span className='info-car-make'>{car.make}</span>
+        <span className='info-car-year'>{car.year}</span>
       </div>
       <table className='info-table'>
-        <th>
-          Car's info
-        </th>
-        <tr>
-          <td>Fuel type</td>
-          <td className='info-value'>{fuel_type}</td>
-        </tr>
-        <tr>
-          <td>Drive</td>
-          <td className='info-value'>{drive}</td>
-        </tr>
-        <tr>
-          <td>Cylinders</td>
-          <td className='info-value'>{cylinders}</td>
-        </tr>
-        <tr>
-          <td>Transmission</td>
-          <td className='info-value'>{transmission == 'a' ? 'automatic' : 'manual'}</td>
-        </tr>
-        <tr>
-          <td>Fuel consumption (City)</td>
-          <td className='info-value'>{parseInt(city_mpg * 0.425143707)} Km/L</td>
-        </tr>
-        <tr>
-          <td>Fuel consumption (Highway)</td>
-          <td className='info-value'>{parseInt(highway_mpg * 0.425143707)} Km/L</td>
-        </tr>
+        <thead>
+          <tr>
+            <th colSpan={2} className='table-title'>
+              Car's info
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Fuel type</td>
+            <td className='info-value'>{car.fuel_type}</td>
+          </tr>
+          <tr>
+            <td>Drive</td>
+            <td className='info-value drive'>{car.drive}</td>
+          </tr>
+          <tr>
+            <td>Cylinders</td>
+            <td className='info-value'>{car.cylinders}</td>
+          </tr>
+          <tr>
+            <td>Transmission</td>
+            <td className='info-value'>{car.transmission == 'a' ? 'automatic' : 'manual'}</td>
+          </tr>
+          <tr>
+            <td>Fuel consumption (City)</td>
+            <td className='info-value'>{parseInt(car.city_mpg * 0.425143707)} Km/L</td>
+          </tr>
+          <tr>
+            <td>Fuel consumption (Highway)</td>
+            <td className='info-value'>{parseInt(car.highway_mpg * 0.425143707)} Km/L</td>
+          </tr>
+        </tbody>
       </table>
+      </>}
     </div>
   )
 }
